35b1ea445dcb8048a50b31b4dd343aa4bf506ee5
Maxime Pageot <maxime.pageot.313@gmail.com>
2021-09-13 13:23:18 -0400

    jai fait les 5 exercices.

--- exercice.py
+++ exercice.py
@@ -3,23 +3,28 @@
 
 
 def is_even_len(string: str) -> bool:
-    return False
+    return not(bool(len(string)%2))
 
 
 def remove_third_char(string: str) -> str:
-    return ""
+    return string[0:2]+string[3:len(string)]
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    return ""
+    return string.replace(old_char,new_char)
 
 
 def get_nb_char(string: str, char: str) -> int:
-    return 0
+    count = 0
+    for lettre in string:
+        if lettre == char:
+            count += 1
+    return count
 
 
 def get_nb_words(sentence: str) -> int:
-    return 0
+    ### on définit un mot comme un ensemble de caracteres nons-espaces qui est délimité par des espaces
+    return len(sentence.split())
 
 
 def main() -> None:
@@ -33,7 +38,7 @@ def main() -> None:
     string = "hello world!"
     print(f"On remplace le caratère w par le caractère z dans la chaine: '{string}'. Résultat: {replace_char(string, 'w', 'z')}")
 
-    print(f"Le nombre d'occurrence de l dans hello est : {get_nb_char(string, 'l')}")
+    print(f"Le nombre d'occurrence de l dans hello world est : {get_nb_char(string, 'l')}")
     
     string = "Baby shark doo doo doo doo doo doo"
     print(f"Le nombre de mots dans la chaine {string} est: {get_nb_words(string)}")
